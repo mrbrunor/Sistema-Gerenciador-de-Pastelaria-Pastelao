1020f31a5dfc3ad9e6e5b9cf7a420d219fcb2469
mrbrunor <mr.bruno_r@live.com>
2014-04-15 21:07:17 -0300

    ItemPedidoDao - CRUD

--- src/com/au/dao/ItemPedidoDao.java
+++ src/com/au/dao/ItemPedidoDao.java
@@ -25,9 +25,14 @@
 package com.au.dao;
 
 import com.au.bd.FabricaConexao;
+import com.au.bean.Caixa;
+import com.au.bean.ItemPedido;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 /**
@@ -37,19 +42,114 @@ import java.util.logging.Logger;
 public class ItemPedidoDao {
     Connection conexao = null;
     
+    //Construtor
     public ItemPedidoDao(){
         conexao = new FabricaConexao().getConexao();
     }
     
+    //Cria Conexão
     public void abreConnection(){
         conexao = new FabricaConexao().getConexao();
     }
     
+    //Finaliza Conexão
     public void fechaConnection() {
         try {
             conexao.close();
         } catch (SQLException ex) {
             Logger.getLogger(ReceitaDao.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+    
+    //CRUD
+    
+    //CREATE    
+    public boolean adicionaItemPedido(ItemPedido novoItemPedido) {
+        String sql = "INSERT INTO itemPedido(idPedido, idProd, qtdProd, totProd) values(?,?,?,?)";
+        PreparedStatement stmt;
+        boolean resultado = false;
+
+        try {
+            stmt = conexao.prepareStatement(sql);
+            stmt.setInt(1, novoItemPedido.getIdPedido());
+            stmt.setInt(2, novoItemPedido.getIdProd());
+            stmt.setInt(3, novoItemPedido.getQtdProd());
+            stmt.setDouble(4, novoItemPedido.getTotProd());
+
+            stmt.execute();
+            stmt.close();
+            conexao.close();
+            resultado = true;
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdutoDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return resultado;
+    }
+    
+    //READ
+    public List<ItemPedido> getLista() {
+        String sql = "SELECT * FROM ItemPedido";
+        PreparedStatement stmt;
+        ResultSet res;
+        List<ItemPedido> listaResItemPedido = new ArrayList<>();
+
+        try {
+            stmt = conexao.prepareStatement(sql);
+            res = stmt.executeQuery();
+            while (res.next()) {
+                ItemPedido itemPedido = new ItemPedido();
+                itemPedido.setIdPedido(res.getInt("idPedido"));
+                itemPedido.setIdProd(res.getInt("idProd"));
+                itemPedido.setQtdProd(res.getInt("qtdProd"));
+                itemPedido.setTotProd(res.getDouble("totProd"));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdutoDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return listaResItemPedido;
+    }
+    
+    //UPDATE    
+    public boolean atualizaItemPedido(ItemPedido novoItemPedido) {
+        String sql = "UPDATE ItemPedido SET qtdProd=?, totProd=? WHERE idPedido=? AND idProd=? ";
+        PreparedStatement stmt;
+        boolean resultado = false;
+
+        try {
+            stmt = conexao.prepareStatement(sql);            
+            stmt.setInt(1, novoItemPedido.getQtdProd());
+            stmt.setDouble(2, novoItemPedido.getTotProd());
+            stmt.setInt(3, novoItemPedido.getIdPedido());
+            stmt.setInt(4, novoItemPedido.getIdProd());
+
+            stmt.execute();
+            stmt.close();
+            conexao.close();
+            resultado = true;
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdutoDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return resultado;
+    }
+    
+    //DELETE
+    public boolean deletaItemPedido(ItemPedido novoItemPedido) {
+        String sql = "DELETE FROM ItemPedido WHERE idPedido=? AND idProd=? ";
+        PreparedStatement stmt;
+        boolean resultado = false;
+
+        try {
+            stmt = conexao.prepareStatement(sql);
+            stmt.setInt(1, novoItemPedido.getIdPedido());
+            stmt.setInt(2, novoItemPedido.getIdProd());
+            
+            stmt.execute();
+            stmt.close();
+            conexao.close();
+            resultado = true;
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdutoDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return resultado;
     }
 }
