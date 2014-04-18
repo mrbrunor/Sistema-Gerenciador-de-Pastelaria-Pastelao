27fd0c1a1f5cf40daf5f20421abb0d36a3b5f01a
mrbrunor <mr.bruno_r@live.com>
2014-04-18 15:30:32 -0300

    PedidoDao - CRUD

--- src/com/au/dao/PedidoDao.java
+++ src/com/au/dao/PedidoDao.java
@@ -25,9 +25,13 @@
 package com.au.dao;
 
 import com.au.bd.FabricaConexao;
+import com.au.bean.Pedido;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 /**
@@ -37,19 +41,130 @@ import java.util.logging.Logger;
 public class PedidoDao {
     Connection conexao = null;
     
+    //Construtor
     public PedidoDao(){
         conexao = new FabricaConexao().getConexao();
     }
     
+    //Cria Conexão
     public void abreConnection(){
         conexao = new FabricaConexao().getConexao();
     }
     
+    //Finaliza Conexão
     public void fechaConnection() {
         try {
             conexao.close();
         } catch (SQLException ex) {
             Logger.getLogger(ReceitaDao.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+    
+    //CRUD
+    //CREATE    
+    public boolean adicionaPedido(Pedido novoPedido) {
+        String sql = "INSERT INTO Pedido(numPedido, dataPedido, horaPedido, idCaixa, subTotPedido, descPedido, totPedido, formaPagtoPedido, estadoPedido) VALUES(?,?,?,?,?,?,?,?,?)";
+        PreparedStatement stmt;
+        boolean resultado = false;
+
+        try {
+            stmt = conexao.prepareStatement(sql);
+            stmt.setInt(1, novoPedido.getNumPedido());
+            stmt.setDate(2, novoPedido.getDataPedido());
+            stmt.setTime(3, novoPedido.getHoraPedido());
+            stmt.setInt(4, novoPedido.getIdCaixa());
+            stmt.setDouble(5, novoPedido.getSubTotPedido());
+            stmt.setDouble(6, novoPedido.getDescPedido());
+            stmt.setDouble(7, novoPedido.getTotPedido());
+            stmt.setString(8, novoPedido.getFormaPagtoPedido());
+            stmt.setString(9, novoPedido.getEstadoPedido());
+
+            stmt.execute();
+            stmt.close();
+            conexao.close();
+            resultado = true;
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdutoDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return resultado;
+    }
+
+    //READ
+    public List<Pedido> getLista() {
+        String sql = "SELECT * FROM Pedido";
+        PreparedStatement stmt;
+        ResultSet res;
+        List<Pedido> listaResPedido = new ArrayList<>();
+
+        try {
+            stmt = conexao.prepareStatement(sql);
+            res = stmt.executeQuery();
+            while (res.next()) {
+                Pedido pedido = new Pedido();
+                pedido.setIdPedido(res.getInt("idPedido"));
+                pedido.setNumPedido(res.getInt("numPedido"));
+                pedido.setDataPedido(res.getDate("dataPedido"));
+                pedido.setHoraPedido(res.getTime("horaPedido"));
+                pedido.setIdCaixa(res.getInt("idCaixa"));
+                pedido.setSubTotPedido(res.getDouble("subTotPedido"));
+                pedido.setDescPedido(res.getDouble("descPedido"));
+                pedido.setTotPedido(res.getDouble("totPedido"));
+                pedido.setFormaPagtoPedido(res.getString("formaPagtoPedido"));
+                pedido.setEstadoPedido(res.getString("estadoPedido"));
+                listaResPedido.add(pedido);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdutoDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return listaResPedido;
+    }
+    
+    //UPDATE
+    public boolean atualizaPedido(Pedido novoPedido) {
+        String sql = "UPDATE Pedido SET numPedido=?, dataPedido=?, horaPedido=?, idCaixa=?, subTotPedido=?, descPedido=?, totPedido=?, formaPagtoPedido=?, estadoPedido=? WHERE idPedido=?";
+        PreparedStatement stmt;
+        boolean resultado = false;
+
+        try {
+            stmt = conexao.prepareStatement(sql);
+            stmt.setInt(1, novoPedido.getNumPedido());
+            stmt.setDate(2, novoPedido.getDataPedido());
+            stmt.setTime(3, novoPedido.getHoraPedido());
+            stmt.setInt(4, novoPedido.getIdCaixa());
+            stmt.setDouble(5, novoPedido.getSubTotPedido());
+            stmt.setDouble(6, novoPedido.getDescPedido());
+            stmt.setDouble(7, novoPedido.getTotPedido());
+            stmt.setString(8, novoPedido.getFormaPagtoPedido());
+            stmt.setString(9, novoPedido.getEstadoPedido());
+            stmt.setInt(10, novoPedido.getIdPedido());
+
+            stmt.execute();
+            stmt.close();
+            conexao.close();
+            resultado = true;
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdutoDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return resultado;
+    }
+    
+    //DELETE
+    public boolean deletaPedido(Pedido novoPedido) {
+        String sql = "DELETE FROM Pedido WHERE idPedido=?";
+        PreparedStatement stmt;
+        boolean resultado = false;
+
+        try {
+            stmt = conexao.prepareStatement(sql);
+            stmt.setInt(1, novoPedido.getIdPedido());
+
+            stmt.execute();
+            stmt.close();
+            conexao.close();
+            resultado = true;
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdutoDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return resultado;
     }
 }
