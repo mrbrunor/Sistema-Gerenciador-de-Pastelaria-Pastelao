93e7afb6e5a882efd78c29aac6b8e8126cd8c074
mrbrunor <mr.bruno_r@live.com>
2014-06-02 00:41:14 -0300

    Melhoria do Relatório de fechamento

--- src/com/au/gui/listener/FechamentoActionListener.java
+++ src/com/au/gui/listener/FechamentoActionListener.java
@@ -44,11 +44,17 @@ import javax.swing.JOptionPane;
  * @author BrunoRicardo
  */
 public class FechamentoActionListener implements ActionListener, KeyListener {
-    
+
     private final TelaFechamentoCaixa frm;
     private Caixa caixa;
     private double dinheiroCaixa;
     private double descontoTotal = 0;
+    private int qtdPedDinheiro = 0;
+    private int qtdPedCred = 0;
+    private int qtdPedDeb = 0;
+    private int qtdPedVale = 0;
+    private int qtdPedCanc = 0;
+    private int qtdPedDesc = 0;
 
     /**
      *
@@ -67,8 +73,8 @@ public class FechamentoActionListener implements ActionListener, KeyListener {
     }
 
     /**
-     * Função responsavel por inicializar os valores dos campos, e chama as funções para
-     * calcular a quantidade vendida por forma de pagamento.
+     * Função responsavel por inicializar os valores dos campos, e chama as
+     * funções para calcular a quantidade vendida por forma de pagamento.
      */
     public void inicializaCampos() {
         frm.getTextoValorFundoDeCaixa().setText(String.format("R$: %.2f", caixa.getFundoCaixa()));
@@ -76,8 +82,10 @@ public class FechamentoActionListener implements ActionListener, KeyListener {
         calculaReducoes();
         calculaTipoPagamento();
     }
+
     /**
-     * Função responsavel por caulcular o valor de cada Tipo de forma de Pagamento
+     * Função responsavel por caulcular o valor de cada Tipo de forma de
+     * Pagamento
      */
     public void calculaTipoPagamento() {
         if (caixa.getPedidos() != null && !caixa.getPedidos().isEmpty()) {
@@ -85,21 +93,28 @@ public class FechamentoActionListener implements ActionListener, KeyListener {
             double totalCredito = 0;
             double totalDebito = 0;
             double totalVale = 0;
-            
+
             for (int i = 0; i < caixa.getPedidos().size(); i++) {
                 if ("Finalizado".equals(caixa.getPedidos().get(i).getEstadoPedido())) {
                     if ("Dinheiro".equals(caixa.getPedidos().get(i).getFormaPagamento().getTipoFormaPgto())) {
                         totalDinheiro = totalDinheiro + caixa.getPedidos().get(i).getTotPedido();
+                        qtdPedDinheiro += 1;
                     } else if ("Credito".equals(caixa.getPedidos().get(i).getFormaPagamento().getTipoFormaPgto())) {
                         totalCredito = totalCredito + caixa.getPedidos().get(i).getTotPedido();
+                        qtdPedCred += 1;
                     } else if ("Debito".equals(caixa.getPedidos().get(i).getFormaPagamento().getTipoFormaPgto())) {
                         totalDebito = totalDebito + caixa.getPedidos().get(i).getTotPedido();
+                        qtdPedDeb += 1;
                     } else if ("Vale".equals(caixa.getPedidos().get(i).getFormaPagamento().getTipoFormaPgto())) {
                         totalVale = totalVale + caixa.getPedidos().get(i).getTotPedido();
+                        qtdPedVale += 1;
                     }
-                    if(caixa.getPedidos().get(i).getDescPedido() != 0){
+                    if (caixa.getPedidos().get(i).getDescPedido() != 0) {
                         descontoTotal += caixa.getPedidos().get(i).getDescPedido();
+                        qtdPedDesc += 1;
                     }
+                } else {
+                    qtdPedCanc += 1;
                 }
             }
             frm.getTextoValorDinheiro().setText(String.format("R$: %.2f", totalDinheiro));
@@ -113,6 +128,7 @@ public class FechamentoActionListener implements ActionListener, KeyListener {
             frm.getTextoValorValeRefeicao().setText("R$: 0,00");
         }
     }
+
     /**
      * Função responsavel por calcular as retiradas de caixa
      */
@@ -128,14 +144,16 @@ public class FechamentoActionListener implements ActionListener, KeyListener {
             frm.getTextoValorTotalRetiradas().setText("R$: 0,00");
         }
         frm.getTextoValorTotalDeReducoes().setText(String.format("R$: %.2f", totalDesp));
-        
+
         frm.getTextoValorRetiradas().setText(String.format("R$: %.2f", totalDesp));
         frm.getTextoValorFaturamentos().setText(String.format("R$: %.2f", caixa.getTotalCaixa()));
         frm.getTextoValorTotalCaixa().setText(String.format("R$: %.2f", caixa.getTotalCaixa() - totalDesp));
-        
-    }    
+
+    }
+
     /**
-     * Função responsavel por adicionar este action listenner aos botões e campos da TelaFechamentoCaixa
+     * Função responsavel por adicionar este action listenner aos botões e
+     * campos da TelaFechamentoCaixa
      */
     public void adicionaListener() {
         frm.getBotaoCancelarFechamentoDeCaixa().addActionListener(this);
@@ -152,12 +170,14 @@ public class FechamentoActionListener implements ActionListener, KeyListener {
         frm.getCampoMoedaUmReal().addKeyListener(this);
         frm.getCampoMoedaVinteCincoCentavos().addKeyListener(this);
     }
+
     /**
-     * Função responsavel por calcular o valor de cada tipo de Moeda ou Cedula Digitada no campo correspondente da Tela.
+     * Função responsavel por calcular o valor de cada tipo de Moeda ou Cedula
+     * Digitada no campo correspondente da Tela.
      */
     public void calculaDinheiroEmCaixa() {
         dinheiroCaixa = 0;
-        
+
         if (!"".equals(frm.getCampoMoedaCincoCentavos().getText())) {
             dinheiroCaixa += Integer.valueOf((frm.getCampoMoedaCincoCentavos().getText())) * 0.05;
         }
@@ -191,9 +211,10 @@ public class FechamentoActionListener implements ActionListener, KeyListener {
         if (!"".equals(frm.getCampoCedulaVinteReais().getText())) {
             dinheiroCaixa += Double.valueOf((frm.getCampoCedulaVinteReais().getText())) * 20;
         }
-        
+
         frm.getTextoValorTotalEmCaixa().setText(String.format("R$: %.2f", dinheiroCaixa));
     }
+
     /**
      * Função responsavel por Efetuar o Fechamento do caixa
      */
@@ -209,7 +230,7 @@ public class FechamentoActionListener implements ActionListener, KeyListener {
         new DAO<>(Caixa.class).atualiza(caixa);
         //Seta o atributo "fechou" como true, para que o metodo que chamouo o Fechamento de caixa possa saber que o fechamento foi efetuado com Sucesso
         TelaFechamentoCaixa.setFechou(true);
-        
+
         //Cria e Imprime o Relatório do caixa fechado
         SimpleDateFormat formatador = new SimpleDateFormat("dd/MM/yyyy");
         String dataStr = formatador.format(caixa.getDataFechamentoCaixa());
@@ -217,7 +238,17 @@ public class FechamentoActionListener implements ActionListener, KeyListener {
         bematech.detectaImpressoras("Caixa");
         if (Bematech.impressora != null) {
             bematech.imprime("\n\tFechamento de Caixa\n");
-            bematech.imprime("\n\tFuncionario: " + caixa.getFuncionario().getNomeFunc());
+            bematech.imprime("\nFuncionario: " + caixa.getFuncionario().getNomeFunc());
+            bematech.imprime("\n");
+            bematech.imprime("\nQuantidade de Pedidos: " + qtdPedCred + qtdPedDeb + qtdPedDinheiro + qtdPedVale);
+            bematech.imprime("\nDinheiro: " + qtdPedDinheiro);
+            bematech.imprime("\nCredito: " + qtdPedCred);
+            bematech.imprime("\nDebito: " + qtdPedDeb);
+            bematech.imprime("\nVale: " + qtdPedVale);
+            bematech.imprime("\n--------------------------------------------------------------\n");
+            bematech.imprime("\n");
+            bematech.imprime("\nFundo de Caixa: " + caixa.getFundoCaixa());
+            bematech.imprime("\n");
             bematech.imprime(dataStr + "                    " + caixa.getDataFechamentoCaixa());
             bematech.imprime("\nDinheiro: " + frm.getTextoValorDinheiro().getText());
             bematech.imprime("\nCredito: " + frm.getTextoValorCartaoDeCredito().getText());
@@ -230,13 +261,15 @@ public class FechamentoActionListener implements ActionListener, KeyListener {
             bematech.imprime("\nTotal Caixa: " + frm.getTextoValorTotalCaixa());
             bematech.imprime("\n--------------------------------------------------------------\n");
             bematech.imprime("\n");
-            bematech.imprime("\nLista de Retiradas\n");
-            
+
             List<Despesa> despesas = new DespesaDao().getLista(caixa.getIdCaixa());
+
             if (despesas != null && !despesas.isEmpty()) {
-                for (int i = 0; despesas.size() > i ;i++) {
+                bematech.imprime("\nQuantidade de Retiradas: " + despesas.size());
+                bematech.imprime("\n\nLista de Retiradas\n");
+                for (int i = 0; despesas.size() > i; i++) {
                     bematech.imprime("\n");
-                    bematech.imprime("\nDespesa " + (i+1));
+                    bematech.imprime("\nDespesa " + (i + 1));
                     bematech.imprime("\nMotivo: " + despesas.get(i).getDescDesp());
                     bematech.imprime("\nValor: " + despesas.get(i).getValorDesp());
                     bematech.imprime("\n");
@@ -244,20 +277,56 @@ public class FechamentoActionListener implements ActionListener, KeyListener {
                 }
             } else {
                 bematech.imprime("\nSem Retiradas\n");
+            }            
+            if(caixa.getPedidos() != null && caixa.getPedidos().size() > 0){
+                bematech.imprime("\n--------------------------------------------------------------\n");
+                bematech.imprime("\nQuantidade de Descontos: " + qtdPedDesc);
+                bematech.imprime("\n\nLista de Descontos\n");
+                for (int i = 0; caixa.getPedidos().size() > i; i++){
+                    if(caixa.getPedidos().get(i).getDescPedido() > 0){
+                        bematech.imprime("\nNumero do Pedido: " + caixa.getPedidos().get(i).getNumPedido());
+                        bematech.imprime("\nValor do Pedido: " + caixa.getPedidos().get(i).getSubTotPedido());
+                        bematech.imprime("\nValor do Desconto: " + caixa.getPedidos().get(i).getDescPedido());
+                        bematech.imprime("\n");
+                        bematech.imprime("\n");
+                    }
+                }
+                bematech.imprime("\n--------------------------------------------------------------\n");
+                bematech.imprime("\n\nTotal de Descontos: " + String.format("R$: %.2f", descontoTotal));
+            } else {
+                bematech.imprime("\nSem Descontos\n");
             }
-            bematech.imprime("\n--------------------------------------------------------------\n");
-            bematech.imprime("\nTotal de Descontos: " + String.format("R$: %.2f", descontoTotal));
             
+            if(caixa.getPedidos() != null && caixa.getPedidos().size() > 0){
+                bematech.imprime("\n--------------------------------------------------------------\n");
+                bematech.imprime("\nQuantidade de Cancelamentos: " + qtdPedCanc);
+                bematech.imprime("\n\nLista de Cancelamentos\n");
+                double totalCanc = 0;
+                for (int i = 0; caixa.getPedidos().size() > i; i++){
+                    if("Cancelado".equals(caixa.getPedidos().get(i).getEstadoPedido())){
+                        totalCanc += caixa.getPedidos().get(i).getSubTotPedido();
+                        bematech.imprime("\nNumero do Pedido: " + caixa.getPedidos().get(i).getNumPedido());
+                        bematech.imprime("\nValor do Pedido: " + caixa.getPedidos().get(i).getSubTotPedido());
+                        bematech.imprime("\n");
+                        bematech.imprime("\n");
+                    }
+                }
+                bematech.imprime("\n--------------------------------------------------------------\n");
+                bematech.imprime("\n\nTotal de Cancelamentos: " + String.format("R$: %.2f", totalCanc));
+            } else {
+                bematech.imprime("\nSem Cancelamentos\n");
+            }
             
+
         } else {
             JOptionPane.showMessageDialog(frm, "Impressora Caixa não foi encontrada. O relatório de caixa não será impresso.");
         }
     }
-    
+
     /**
-     * 
-     * @param event 
-     * Função responsavel por pegar o ActionEvent e executar a função apropriada
+     *
+     * @param event Função responsavel por pegar o ActionEvent e executar a
+     * função apropriada
      */
     @Override
     public void actionPerformed(ActionEvent event) {
@@ -271,29 +340,28 @@ public class FechamentoActionListener implements ActionListener, KeyListener {
                 break;
         }
     }
+
     /**
-     * 
-     * @param e 
-     * Implementação obrigatória, sem uso nesta classe
+     *
+     * @param e Implementação obrigatória, sem uso nesta classe
      */
     @Override
-    public void keyTyped(KeyEvent e) {        
+    public void keyTyped(KeyEvent e) {
     }
+
     /**
-     * 
-     * @param e 
-     * Implementação obrigatória, sem uso nesta classe
+     *
+     * @param e Implementação obrigatória, sem uso nesta classe
      */
     @Override
-    public void keyPressed(KeyEvent e) {        
+    public void keyPressed(KeyEvent e) {
     }
-    
+
     /**
-     * 
-     * @param e 
-     * Função responsavel por chamar a Função calculaDinheiroEmCaixa 
-     *  toda vez que uma tecla for "Despressionada" em um dos campos da 
-     *  Seção "Dinheiro em caixa"
+     *
+     * @param e Função responsavel por chamar a Função calculaDinheiroEmCaixa
+     * toda vez que uma tecla for "Despressionada" em um dos campos da Seção
+     * "Dinheiro em caixa"
      */
     @Override
     public void keyReleased(KeyEvent e) {
