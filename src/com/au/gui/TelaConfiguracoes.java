164f86544a114467cd24bffdb38d4c4a605a32f6
Bruno_R <mr.bruno_r@live.com>
2017-11-15 21:52:15 -0200

    Correção Bug
    Corrigido o bug que permitia a tela de configurações ficar atras da tela de venda.

--- src/com/au/gui/TelaConfiguracoes.java
+++ src/com/au/gui/TelaConfiguracoes.java
@@ -40,7 +40,8 @@ public class TelaConfiguracoes extends javax.swing.JDialog {
     /**
      * Creates new form TelaConfiguracoes
      */
-    public TelaConfiguracoes() {
+    public TelaConfiguracoes(java.awt.Frame parent, boolean modal) {
+        super(parent, modal);
         initComponents();
         try {
             prop = getProp();
@@ -434,42 +435,6 @@ public class TelaConfiguracoes extends javax.swing.JDialog {
             System.out.println("Houve erro ao salvar as Configurações");
         }
     }//GEN-LAST:event_botaoSalvarConfiguracoesActionPerformed
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Windows".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TelaConfiguracoes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TelaConfiguracoes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TelaConfiguracoes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TelaConfiguracoes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new TelaConfiguracoes().setVisible(true);
-            }
-        });
-    }
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField CampoNomeCozinha;
     private javax.swing.JButton botaCancelarConfiguracoes;
